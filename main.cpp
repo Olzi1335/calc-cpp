b2f71c8941130275f74ba64646c1159ec0755e37
et <egorarteg@proton.me>
2025-06-04 12:02:16 +0200

    added "d"

--- main.cpp
+++ main.cpp
@@ -7,7 +7,11 @@ int main() {
     //    setlocale(LC_ALL, "Russian");
 
     float number_1, number_2, result = 0;
-    char action= '+';
+    char action;
+
+    cout << "Please enter the action ( /, *, +, -, d) : ";
+    cin >> action;
+    cout << endl << endl;
 
     cout << "Please enter the number 1: ";
     cin >> number_1;
@@ -17,17 +21,40 @@ int main() {
     cin >> number_2;
     cout << endl;
 
-    cout << "Please enter the action ( /, *, +, -) : ";
-    cin >> action;
-    cout << endl << endl;
-
-    cout << number_1 << " " << action << " " << number_2 << " = ";
+    if (action == '+' || action == '-' || action == '*' || action == '/') {
+        cout << number_1 << " " << action << " " << number_2 << " = ";
+    }
+    if (action == 'd') {
+        cout << number_1 << " [" << number_2 << "] = ";
+    }
 
     switch (action) {
         case '+': result = number_1 + number_2; break;
         case '-': result = number_1 - number_2; break;
         case '*': result = number_1 * number_2; break;
-        case '/': result = number_1 / number_2; break;
+        case '/':
+            if (number_2 == 0) {
+                cout << "Error: Division by zero" << endl;
+                return 1;
+            }
+            result = number_1 / number_2;
+            break;
+        case 'd':
+            if (number_2 >= 1001) {
+                cout << number_2 << " is too large";
+                return 10;
+            }
+            if (number_2 <= 1000) {
+                float counter = 0;
+                result = number_1;
+
+                while (counter < number_2) {
+                    result *= number_1;
+                    counter++;
+                }
+            }
+            break;
+
         default: cout << "Invalid input" << endl;
             return 5;
     }
