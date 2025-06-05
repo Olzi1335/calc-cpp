693d724e7b328f2e322859c5760e1005a358f530
et <egorarteg@proton.me>
2025-06-05 22:14:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,65 +1,93 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 
 int main() {
     //    setlocale(LC_ALL, "Russian");
+    string project_name = "calc-cpp";
 
-    long double number_1, number_2, result = 0;
-    char action;
-
-    cout << "Please enter the action ( /, *, +, -, d) : ";
-    cin >> action;
-    cout << endl << endl;
-
-    cout << "Please enter the number 1: ";
-    cin >> number_1;
+    cout << "==========================" << endl;
+    cout << "  Welcome to '" << project_name << "'  " << endl;
+    cout << "==========================" << endl;
     cout << endl;
 
-    cout << "Please enter the number 2: ";
-    cin >> number_2;
-    cout << endl;
+    double number_1, number_2, result = 0.1f;
+    char command;
+    bool quit = false;
+    bool command_right = true;
 
-    if (action == '+' || action == '-' || action == '*' || action == '/') {
-        cout << number_1 << " " << action << " " << number_2 << " = ";
-    }
-    if (action == 'd') {
-        cout << number_1 << " [" << number_2 << "] = ";
-    }
+    while (!quit) {
+        cout << "Please enter the command ('h' for help) : ";
+        cin >> command;
+        cout << endl << endl;
 
-    switch (action) {
-        case '+': result = number_1 + number_2; break;
-        case '-': result = number_1 - number_2; break;
-        case '*': result = number_1 * number_2; break;
-        case '/':
-            if (number_2 == 0) {
-                cout << "Error: Division by zero" << endl;
-                return 1;
-            }
-            result = number_1 / number_2;
-            break;
-        case 'd':
-            if (number_2 >= 1001) {
-                cout << number_2 << " is too large";
-                return 10;
-            }
-            if (number_2 <= 1000) {
-                float counter = 1;
-                result = number_1;
+        if (command == 'h') {
+            cout << "Enter '+' for addition\n"\
+            << "Enter '-' for subtraction\n"\
+            << "Enter '*' for multiplication\n"\
+            << "Enter '/' for division\n"\
+            << "Enter 'd' for exponentiation\n"\
+            << "Enter 'q' for quit\n"\
+            << endl;
+            continue;
+        }
+        if (command == 'q') {
+            quit = true;
+            continue;
 
-                while (counter < number_2) {
-                    result *= number_1;
-                    counter++;
+        }
+        cout << "Please enter the number 1: ";
+        cin >> number_1;
+        cout << endl;
+
+        cout << "Please enter the number 2: ";
+        cin >> number_2;
+        cout << endl;
+
+        if (command == '+' || command == '-' || command == '*' || command == '/') {
+            cout << number_1 << " " << command << " " << number_2 << " = ";
+        }
+        if (command == 'd') {
+            cout << number_1 << " [" << number_2 << "] = ";
+        }
+
+        switch (command) {
+            case '+': result = number_1 + number_2;
+                break;
+            case '-': result = number_1 - number_2;
+                break;
+            case '*': result = number_1 * number_2;
+                break;
+            case '/':
+                if (number_2 == 0) {
+                    cout << "Error: Division by zero" << endl;
+                    return 1;
                 }
-            }
-            break;
+                result = number_1 / number_2;
+                break;
+            case 'd':
+                if (number_2 >= 1001) {
+                    cout << number_2 << " is too large";
+                    return 10;
+                }
+                if (number_2 <= 1000) {
+                    float counter = 1;
+                    result = number_1;
 
-        default: cout << "Invalid input" << endl;
-            return 5;
+                    while (counter < number_2) {
+                        result *= number_1;
+                        counter++;
+                    }
+                }
+                break;
+            case 'h': break;
+            default: cout << "Invalid input" << endl;
+        }
+
+        cout << fixed << result << endl;
+
+        return 0;
     }
-
-    cout << fixed << result << endl;
-
-    return 0;
 }
